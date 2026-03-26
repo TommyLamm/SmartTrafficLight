d72ecbd13f223a113147d17167c559e419aa68a2
ongakuen86 <129674437+ongakuen86@users.noreply.github.com>
2026-03-26 15:47:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -206,7 +206,7 @@ def index():
             </div>
             <iframe
               id="editor-frame"
-              src="http://stledit.gyke.net/"
+              src="http://127.0.0.1:5001/"
               style="width:100%;height:100%;border:none;"
               title="Logic Editor"
             ></iframe>
