f666ed7c091d27a94295353d540a5e33e24eb50a
TommyLamm <lamyuingai@gmail.com>
2026-03-26 20:14:06 +0800

    Modify model and enhance person classification logic

--- smart_traffic/config.py
+++ smart_traffic/config.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 LOGIC_PATH = os.path.join(BASE_DIR, 'logic.py')
 
 CAR_MODEL_PATH = os.path.join(BASE_DIR, 'yolov8n.pt')
-PERSON_MODEL_PATH = os.path.join(BASE_DIR, 'person_wheelchair_personWheelchair.pt')
+PERSON_MODEL_PATH = os.path.join(BASE_DIR, 'person_wheelchair_personWheelchairV2.pt')
 
 XOR_KEY = b"MyIoTKey2026"
 STREAM_ONLINE_TTL_SEC = 5.0
