4e72028147c77bd83debba3267b8b2b18e87badb
AndyFung1233 <107395601+AndyFung1233@users.noreply.github.com>
2026-03-31 04:20:43 +0800

    Include Licence Plate Detection Model in imports
    Added loading of Licence Plate Detection Model.

--- smart_traffic/models.py
+++ smart_traffic/models.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-from .config import CAR_MODEL_PATH, PERSON_MODEL_PATH
+from .config import CAR_MODEL_PATH, PERSON_MODEL_PATH, PLATE_MODEL_PATH
 
 
 print("Loading Car Detection Model...")
@@ -8,3 +8,6 @@ car_model = YOLO(CAR_MODEL_PATH)
 
 print("Loading Person/Wheelchair Detection Model...")
 person_model = YOLO(PERSON_MODEL_PATH)
+
+print("Loading Licence Plate Detection Model...")
+plate_model = YOLO(PLATE_MODEL_PATH)
